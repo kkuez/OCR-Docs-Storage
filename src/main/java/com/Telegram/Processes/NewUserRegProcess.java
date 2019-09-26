fb87233993c37108e5249e736aae9a8498359875
Marcel <doerping@gmx.de>
2019-09-26 19:57:38 +0200

    ChatId will now be stored for user.

--- src/main/java/com/Telegram/Processes/NewUserRegProcess.java
+++ src/main/java/com/Telegram/Processes/NewUserRegProcess.java
@@ -18,8 +18,8 @@ public class NewUserRegProcess extends Process {
     public void performNextStep(String arg, Update update) {
         if(arg.equals(ObjectHub.getInstance().getProperties().getProperty("pwForNewUsers"))){
             BotUtil.sendMsg(update.getMessage().getChatId() + "", "Willkommen :)", getBot());
-            DBUtil.executeSQL("insert into AllowedUsers(id, name) Values (" + update.getMessage().getFrom().getId() + ", '" +
-                    update.getMessage().getFrom().getFirstName() + "')");
+            DBUtil.executeSQL("insert into AllowedUsers(id, name, chatId) Values (" + update.getMessage().getFrom().getId() + ", '" +
+                    update.getMessage().getFrom().getFirstName() + "', " + update.getMessage().getChatId() + ")");
             ObjectHub.getInstance().setAllowedUsersMap(DBUtil.getAllowedUsersMap());
             getBot().process = null;
         }
