d587a788e4002659369fd7d90a9cfd636c6e3132
Marcel <doerping@outlook.com>
2019-09-27 19:26:28 +0200

    Implemented one ThreadClass to perform LaterTasks.

---
+++ src/main/java/com/Misc/TaskHandling/UpdateTask.java
@@ -0,0 +1,54 @@
+package com.Misc.TaskHandling;
+
+import com.Telegram.Bot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class UpdateTask implements Task {
+    Update update;
+
+    Bot bot;
+
+    boolean successFullyExecuted;
+
+
+    public UpdateTask(Update update, Bot bot){
+        this.update = update;
+        this.bot = bot;
+        successFullyExecuted = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            bot.processUpdateReceveived(update);
+            successFullyExecuted = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //GETTER SETTER
+    public Update getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Update update) {
+        this.update = update;
+    }
+
+    public Bot getBot() {
+        return bot;
+    }
+
+    public void setBot(Bot bot) {
+        this.bot = bot;
+    }
+
+    public boolean isSuccessFullyExecuted() {
+        return successFullyExecuted;
+    }
+
+    public void setSuccessFullyExecuted(boolean successFullyExecuted) {
+        this.successFullyExecuted = successFullyExecuted;
+    }
+}
---
+++ src/main/java/com/Taskshub.java
@@ -0,0 +1,60 @@
+package com;
+
+import com.Misc.TaskHandling.Task;
+import com.Utils.LogUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Taskshub implements Runnable {
+
+    private List<Task> tasksToDo = new ArrayList<>();
+
+    private int performTimesPerHour;
+
+    private boolean loopActive;
+
+    public Taskshub(int performTimesPerHour){
+        this.performTimesPerHour = performTimesPerHour;
+    }
+
+    private void loop() {
+        loopActive = true;
+        while (loopActive){
+            try {
+                Thread.sleep(60000 / performTimesPerHour);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LogUtil.log("System: Trying to perform LaterTasks (" + tasksToDo.size() + " in List)");
+           for(Task task : tasksToDo){
+               task.run();
+               if(task.isSuccessFullyExecuted()){
+                   tasksToDo.remove(task);
+               }
+           }
+        }
+    }
+
+    @Override
+    public void run() {
+        loop();
+    }
+
+    //GETTER SETTER
+
+
+
+    public boolean isLoopActive() {
+        return loopActive;
+    }
+
+    public void setLoopActive(boolean loopActive) {
+        this.loopActive = loopActive;
+    }
+
+    public List<Task> getTasksToDo() {
+        return tasksToDo;
+    }
+
+}
