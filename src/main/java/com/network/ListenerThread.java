1d8da55f6a457b96f8c700e53ec0c2a391f094d6
Marcel <doerping@gmx.de>
2019-12-23 11:04:44 +0100

    Added parsing of QR info for mappable items.

--- src/main/java/com/network/ListenerThread.java
+++ src/main/java/com/network/ListenerThread.java
@@ -8,12 +8,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ListenerThread extends Thread {
 
     public static boolean networkRun = true;
     private static String addListCMD = "<addList>";
+    private static String addListCMDItemNr = "<addList>Item#";
     private Bot bot;
     private static int socketPort = 55555;
     public ListenerThread(Bot bot){
@@ -41,10 +43,18 @@ public class ListenerThread extends Thread {
 
     private void processIncomingStream(String incomingString){
         LogUtil.log("Processing incoming Stream: " + incomingString);
-        if(incomingString.startsWith(addListCMD)){
-            String item  = incomingString.replace(addListCMD, "").replace("_", " ");
+        if(incomingString.startsWith(addListCMDItemNr)){
+            Map<Integer, String> itemMap = DBUtil.getQRItemMap();
+            int itemNumber = Integer.parseInt(incomingString.replace(addListCMDItemNr, ""));
+            String item = itemMap.get(itemNumber);
             bot.getShoppingList().add(item);
             DBUtil.executeSQL("insert into ShoppingList(item) Values ('" + item + "')");
+        }else {
+            if (incomingString.startsWith(addListCMD)) {
+                String item = incomingString.replace(addListCMD, "").replace("_", " ");
+                bot.getShoppingList().add(item);
+                DBUtil.executeSQL("insert into ShoppingList(item) Values ('" + item + "')");
+            }
         }
     }
 }
