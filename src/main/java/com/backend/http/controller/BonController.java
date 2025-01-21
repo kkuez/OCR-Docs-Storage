5d6adf9f042359d8cb75d0ca80e70f7ce66dc431
kkuez <doerping@outlook.com>
2025-01-21 09:52:33 +0100

    Added new Bon insertion very dirtyly.

--- src/main/java/com/backend/http/controller/BonController.java
+++ src/main/java/com/backend/http/controller/BonController.java
@@ -2,6 +2,7 @@ package com.backend.http.controller;
 
 import com.backend.BackendFacadeImpl;
 import com.data.Bon;
+import com.data.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class BonController extends Controller {
@@ -80,8 +81,18 @@ public class BonController extends Controller {
 
     @RequestMapping(value = BON + "/send", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> send(@RequestParam Map<String, String> map, @RequestParam("file") MultipartFile[] submissions, HttpServletRequest request) {
+        String betrag = map.get("betrag").replace(",", ".");
+
         MultipartFile uploadedFile = submissions[0];
         String originalFilename = uploadedFile.getOriginalFilename();
+        boolean betragGiven = !betrag.isEmpty();
+        boolean fileUploaded = !originalFilename.isEmpty();
+
+        if(!betragGiven || ! fileUploaded) {
+            return ResponseEntity.ok("Da fehlt wohl was (Betrag ohne Buchstaben!).");
+        }
+
+        float betragFloat = Float.parseFloat(betrag);
         try (InputStream inputOfStream = uploadedFile.getInputStream()) {
             byte[] bytesOfUploadedFile = inputOfStream.readAllBytes();
 
@@ -91,70 +102,111 @@ public class BonController extends Controller {
             if (!targetFile.exists()) {
                 targetFile.createNewFile();
             }
-            Path path = Files.write(targetFile.toPath(), bytesOfUploadedFile);
+            File createdFile = Files.write(targetFile.toPath(), bytesOfUploadedFile).toFile();
+            File copiedToArchiveFile = backendFacade.copyToArchive(createdFile, true);
 
-
+            Bon bon = new Bon(backendFacade.getIdForNextDocument(), backendFacade.getAllowedUsers().get(map.get("userid")), copiedToArchiveFile, betragFloat, UUID.randomUUID());
+            backendFacade.getDBDAO().insertBon(bon);
         } catch (Exception e) {
             logger.error(COULD_NOT_PARSE_INCOMING_BON, e);
             return ResponseEntity.ok(COULD_NOT_PARSE_INCOMING_BON);
         }
 
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        String userid = parameterMap.get("userid")[0];
+        String passw = parameterMap.get("passw")[0];
+       /* String returnString = "<form action=\"\\bons\\addBonsHTML\\?userid=" + userid + "&passw=" + passw + "\" method=\"get\">" +
+                "<input type=\"submit\" value=\"Go to my link location\"" +
+                "name=\"Submit\" id=\"frm1_submit\" />" +
+                "</form>";*/
+        String returnString = getAddBonStringBuilder(false, false, userid, passw).toString();
 
-    /*    File archivedPic = backendFacade.copyToArchive(newPic, true);
-        Bon bon = new Bon(backendFacade.getIdForNextDocument(), backendFacade.getAllowedUsers().get(userid),
-                archivedPic, sum, UUID.randomUUID());
-        backendFacade.insertBon(bon);*/
-        return ResponseEntity.ok("");
+        return ResponseEntity.ok("Neuer Bon hingezugefügt!" + returnString);
     }
 
     @RequestMapping(value = BON + "/addBonsHTML", method = RequestMethod.GET)
-    public ResponseEntity<String> addBonsHTML(HttpServletRequest request) {
-        String userid;
-        String passw;
+    public ResponseEntity<String> addBonsHTML(HttpServletRequest request, boolean betragFilledFloat, boolean pictureFilled) {
         Map<String, String[]> parameterMap = request.getParameterMap();
-        if (parameterMap.size() != 2 || (!parameterMap.containsKey("userid") || !parameterMap.containsKey("passw"))) {
-            throw new RuntimeException("User ID or password not given!");
-        } else {
-            userid = parameterMap.get("userid")[0];
-            passw = parameterMap.get("passw")[0];
+        String userid = parameterMap.get("userid")[0];
+        String passw = parameterMap.get("passw")[0];
+        betragFilledFloat = !(String.valueOf(parameterMap.get("betrag")).isEmpty());
+        pictureFilled = parameterMap.containsKey("file") && !parameterMap.get("file")[0].isEmpty();
+        if (betragFilledFloat && pictureFilled) {
+            //send(request.getParameterMap(), );
+            return ResponseEntity.ok("");
         }
 
-        Float sum = backendFacade.getSum(userid);
-        List<Bon> lastBons = backendFacade.getLastBons(userid, Integer.valueOf(10));
+        StringBuilder htmlBuilder = getAddBonStringBuilder(betragFilledFloat, pictureFilled, userid, passw);
+        return ResponseEntity.ok(htmlBuilder.toString());
+    }
 
-
+    private StringBuilder getAddBonStringBuilder(boolean betragFilledFloat, boolean pictureFilled, String userid, String passw) {
         StringBuilder htmlBuilder = new StringBuilder("<html><body>");
 
 
         //htmlBuilder.append("<form action=\"\\bon\\send\" name=\"myForm\" enctype=\"text\" method=\"post\">");
-        htmlBuilder.append("<form action=\"\\bon\\send\" name=\"myForm\" enctype=\"multipart/form-data\" method=\"post\">");
+
+
+            htmlBuilder.append("<form action=\"\\bon\\send\" onsubmit=\"this.submit();this.reset();return false;\" name=\"myForm\" id=\"myForm\" enctype=\"multipart/form-data\" method=\"post\">");
+
         htmlBuilder.append("<input type=\"hidden\" name=\"userid\" id=\"userid\" value=\"" + userid + "\">");
         htmlBuilder.append("<input type=\"hidden\" name=\"passw\" id=\"passw\" value=\"" + passw + "\">");
 
         htmlBuilder.append("<p style=\"font-family:arial;\">");
         htmlBuilder.append("<label for=\"betrag\">Betrag:</label>");
-        htmlBuilder.append("<input type=\"text\" name=\"betrag\" id=\"betrag\">");
+        htmlBuilder.append("<input autocomplete=\"one-time-code\" type=\"text\" name=\"betrag\" id=\"betrag\">");
 
         htmlBuilder.append("<br><br><label for=\"file\">Bon-Bild</label><br>");
-        htmlBuilder.append("<input type=\"file\" id=\"file\" accept=\"image/*\" name=\"file\">");
+        htmlBuilder.append("<input autocomplete=\"one-time-code\" type=\"file\" id=\"file\" accept=\"image/*\" name=\"file\">");
         //htmlBuilder.append("<button>Upload</button>");
 
         htmlBuilder.append("</label>");
-        htmlBuilder.append("<br><br><input value=\"Abschicken\" type=\"submit\" onclick=\"send()\">");
+
+        if (!pictureFilled || !betragFilledFloat) {
+            htmlBuilder.append("Da fehlt noch was...");
+        }
+
+        htmlBuilder.append("<br><br><input autocomplete=\"one-time-code\" value=\"Abschicken\" type=\"submit\" onclick=\"send()\">");
 
         htmlBuilder.append("<br><br><label for=\"Bisher\">Bisher:</label>");
         htmlBuilder.append("</form>");
 
+        Float sum = backendFacade.getSum(userid);
+        Map<String, User> allowedUsers = backendFacade.getAllowedUsers();
+        List<String> userIds = allowedUsers.values().stream().map(u -> u.getName()).collect(Collectors.toList());
+        Float sumOfAllUsers = 0F;
+        for (String userId : userIds) {
+            Float sumOfUserId = backendFacade.getSum(userId);
+            sumOfAllUsers += sumOfUserId;
+        }
+
         htmlBuilder.append("</p><p style=\"font-family:arial; color:");
-        if (sum < 0) {
+        if (sum > sumOfAllUsers) {
             htmlBuilder.append("red");
+            htmlBuilder.append(";\">");
+            htmlBuilder.append("-");
         } else {
             htmlBuilder.append("green");
+            htmlBuilder.append(";\">");
+            htmlBuilder.append("+");
         }
-htmlBuilder.append(";\">");
 
-        htmlBuilder.append(sum + "");
+
+        float midSum = sumOfAllUsers / allowedUsers.size();
+
+        float leftSum = Math.abs(sum - midSum);
+
+        htmlBuilder.append(leftSum + "");
+
+        if (!betragFilledFloat) {
+            htmlBuilder.append("</p><p style=\"font-family:arial; color:red;\"><h1>Welcher Betrag? Format muss XX.YY sein</h1>");
+        }
+        if (!pictureFilled) {
+            htmlBuilder.append("</p><p style=\"font-family:arial; color:red;\"><h1>Bild benötigt</h1>");
+        }
+
+
         htmlBuilder.append("</p></body></html>");
-        return ResponseEntity.ok(htmlBuilder.toString());
+        return htmlBuilder;
     }
 }
